53dcd745232078cb03656b7363456ec33f864938
Joost Lubach <joost@yoazt.com>
2015-02-20 10:53:24 +0100

    Using database for candidate lookup

--- app/controllers/CandidatesController.php
+++ app/controllers/CandidatesController.php
@@ -3,8 +3,23 @@
 class CandidatesController extends Controller {
 
   public function stack($parameters) {
-    $data = new CandidatesData();
-    return $data->stack($parameters['province']);
+    $query = $this->em->createQueryBuilder()
+      ->select('c')
+      ->from('Candidate', 'c')
+      ->where('c.province = ?1')
+      ->setParameter(1, $parameters['province'])
+      ->getQuery();
+
+    $candidates = $query->getResult();
+
+    shuffle($candidates);
+    return array_map(function ($candidate) {
+      return array(
+        'id'        => $candidate->getId(),
+        'name'      => $candidate->getName(),
+        'image_url' => $candidate->getImageUrl()
+      );
+    }, $candidates);
   }
 
 }
--- app/controllers/VotesController.php
+++ app/controllers/VotesController.php
@@ -3,8 +3,10 @@
 class VotesController extends Controller {
 
   function create($parameters) {
+    $candidate = $this->em->find('Candidate', $parameters['candidate_id']);
+
     $vote = new Vote();
-    $vote->setCandidateId($parameters['candidate_id']);
+    $vote->setCandidate($candidate);
     $vote->setVote($parameters['vote']);
 
     $this->em->persist($vote);
